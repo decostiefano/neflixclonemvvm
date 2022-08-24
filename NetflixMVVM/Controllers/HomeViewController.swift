b09ab13aacb363c636265de1bc89c2d7d3f4cf5f
decostiefano <decostfn@gmail.com>
2022-08-24 09:04:40 +0700

    update home

---
+++ NetflixMVVM/Controllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  NetflixMVVM
+//
+//  Created by Jerry Purnama Maulid on 22/08/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+
+}
---
+++ NetflixMVVM/Views/MainTabBarViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MainTabBarViewController.swift
+//  NetflixMVVM
+//
+//  Created by Jerry Purnama Maulid on 22/08/22.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+        
+        let vc1 = UINavigationController(rootViewController: ViewController())
+        let vc2 = UINavigationController(rootViewController: ViewController())
+        let vc3 = UINavigationController(rootViewController: ViewController())
+        let vc4 = UINavigationController(rootViewController: ViewController())
+
+        
+        vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc2.tabBarItem.image = UIImage(systemName: "play.circle")
+        vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        
+        vc1.title = "Home"
+        vc2.title = "Coming Soon"
+        vc3.title = "Search"
+        vc4.title = "Downloads"
+        
+        
+        
+        tabBar.tintColor = .label
+        
+        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
+        
+    }
+    
+    
+}
